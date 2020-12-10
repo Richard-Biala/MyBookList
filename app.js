c3c586442f2750ebfb57d78c3eab92699edb8c7c
Richard <rkj128@gmail.com>
2020-12-10 15:00:53 -0800

    Add Book funtion works

--- app.js
+++ app.js
@@ -50,5 +50,23 @@ class UI {
 //Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 //Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    //Prevent actual submit
+    e.preventDefault();
+
+    //get form value
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //Intatiate Book
+    const book = new Book (title, author, isbn);
+
+    //Add Book to UI
+    UI.addBookToList(book);
+
+    console.log(book);
+
+});
 
 //Event: Remove a Bood
